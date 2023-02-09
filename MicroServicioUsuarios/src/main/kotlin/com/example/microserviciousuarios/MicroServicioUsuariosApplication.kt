8152f301c563f26404dbc61e9fb013ddf7f30971
Alejandro López Abad <alexlopezabad13@gmail.com>
2023-02-09 11:45:46 +0100

    Preparacion MicroservicioUsuario

---
+++ MicroServicioUsuarios/src/main/kotlin/com/example/microserviciousuarios/MicroServicioUsuariosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.microserviciousuarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroServicioUsuariosApplication
+
+fun main(args: Array<String>) {
+    runApplication<MicroServicioUsuariosApplication>(*args)
+}
