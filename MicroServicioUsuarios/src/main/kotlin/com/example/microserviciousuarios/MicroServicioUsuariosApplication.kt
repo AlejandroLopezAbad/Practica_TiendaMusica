e5090082b8db8eb8fbd93aace95e81b324342c37
Alejandro López Abad <alexlopezabad13@gmail.com>
2023-02-20 12:59:56 +0100

    Docker Consulta no funciona

--- MicroServicioUsuarios/src/main/kotlin/com/example/microserviciousuarios/MicroServicioUsuariosApplication.kt
+++ MicroServicioUsuarios/src/main/kotlin/com/example/microserviciousuarios/MicroServicioUsuariosApplication.kt
@@ -1,10 +1,25 @@
 package com.example.microserviciousuarios
 
+import com.example.microserviciousuarios.repositories.UsersRepository
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MicroServicioUsuariosApplication
+class MicroServicioUsuariosApplication : CommandLineRunner {
+    @Autowired
+    lateinit var service: UsersRepository
+    override fun run(vararg args: String?) = runBlocking {
+
+    val prueba = service.findAll().toList()
+
+        println(prueba)
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<MicroServicioUsuariosApplication>(*args)
