9b3af7265a89f0c609151579ebf2a5028fc4a71c
Francisco Toribio Respaldo <90746957+frantoribio@users.noreply.github.com>
2023-03-05 10:57:50 +0100

    Actualizando pedidos Ktor

--- pedidos-ktor/build.gradle.kts
+++ pedidos-ktor/build.gradle.kts
@@ -1,11 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
+//KOIN
+val koin_ktor_version: String by project
+val ksp_version: String by project
+val koin_ksp_version: String by project
+val koin_version: String by project
+
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.3"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.0"
+    id("io.ktor.plugin") version "2.2.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
 }
 
 group = "es.tiendamusica"
@@ -17,16 +26,86 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("servicio-pedidos.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
-
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
+}
 dependencies {
+    //testImplementation(kotlin("test"))
+    //Ktor core
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("io.ktor:ktor-server-cors-jvm:2.2.2")
+//Validators Ktor
+    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
+
+    // Para testear métodos suspendidos o corrutinas
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
+    //Kmongo
+    implementation("org.litote.kmongo:kmongo-async:4.7.2")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.7.2")
+
+    //Content negotiation
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    //Serializacion
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+
+    //KOIN
+    implementation("io.insert-koin:koin-ktor:$koin_ktor_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor_version")
+    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
+    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
+
+
+    //MockK
+    testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
 }
+tasks.test {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+ktor {
+    docker {
+        localImageName.set("servicio-pedidos")
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        portMappings.set(
+            listOf(
+                io.ktor.plugin.features.DockerPortMapping(
+                    8080,
+                    8080,
+                    io.ktor.plugin.features.DockerPortMappingProtocol.TCP
+                )
+            )
+        )
+    }
+}
+
+
---
+++ pedidos-ktor/src/main/kotlin/es/tiendamusica/plugins/Swagger.kt
@@ -0,0 +1,7 @@
+package es.tiendamusica.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSwagger() {
+
+}
